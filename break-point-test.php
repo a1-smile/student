21b9d643529a3b99970ced95667c7354d28d09ab
a1-smile <yokohama.3210@gmail.com>
2026-07-24 11:09:16 +0900

    Xdebug 動作確認

---
+++ break-point-test.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <p>test start</p>
+ <p>
+    <?php
+    echo "Hello, World!1";
+    $a = 1;
+    echo "Hello, World!2";
+    $b = 2;
+    echo "Hello, World!3";
+    $c = $a + $b;
+    echo "Hello, World!4";
+    ?>
+ </p>   
+</body>
+</html>
